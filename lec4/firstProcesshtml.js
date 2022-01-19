3a2ff6a608885f3f202b0608d6dbfb38da40bcf5
wickywinner <winner312.com@gmail.com>
2022-01-20 00:12:19 +0530

    lec4

--- lec4/firstProcesshtml.js
+++ lec4/firstProcesshtml.js
@@ -4,10 +4,16 @@ let parse = require("minimist");
 let args= parse(process.argv);
 let fs= require("fs");
 let jsdom = require("jsdom");//It load html and prepare DOM(Document Object Module) for programmer.
+const { div } = require("prelude-ls");
 
 fs.readFile(args.source,"utf-8",function(err,data){
-    let JSDOM = jsdom.JSDOM;
-    let dom = new JSDOM(data);
+    let dom = new jsdom.JSDOM(data);
     let document = dom.window.document;
-
-})
+    let needed_doc = document.querySelectorAll("div.match-info > div.description");
+    //we will get all the div's of class description whose parent class is match-info
+    for(let i=0;i<needed_doc.length;i++)
+    {
+        console.log(needed_doc[i].textContent);
+    }
+    })
+    //
