5b02ed066dce56fcc81a4c0c557f0c66178468c7
wickywinner <winner312.com@gmail.com>
2022-01-23 15:16:01 +0530

    add style in excel sheet

--- lec6/firstexcel.js
+++ lec6/firstexcel.js
@@ -3,18 +3,30 @@
  let args = parse(process.argv);
  let fs= require("fs");
  let excel = require("excel4node");
-const { cellComment } = require("excel4node/distribution/lib/types");
 
  let teamjson = fs.readFileSync(args.source,"utf-8");
  let team = JSON.parse(teamjson);
  let wb = new excel.Workbook();
+ let hs= wb.createStyle({
+     font:{
+         bold:true,
+         underline:true,
+         size:16,
+         color:"red"
+     },
+     fill:{
+         type:"pattern",
+         patternType:"solid",
+         fgColor:"yellow"
+     }
+ })
  for(let i=0;i<team.length;i++)
  {
     let sheet = wb.addWorksheet(team[i].name);
-    sheet.cell(1,1).string("Rank");
+    sheet.cell(1,1).string("Rank").style(hs);
     sheet.cell(1,2).number(team[i].rank);
-    sheet.cell(2,1).string("Vs");
-    sheet.cell(2,2).string("Result");
+    sheet.cell(2,1).string("Vs").style(hs);
+    sheet.cell(2,2).string("Result").style(hs);
     for(let j=0;j<team[i].matches.length;j++)
     {
         sheet.cell(j+3,1).string(team[i].matches[j].vs);
